8a6bc4ec869f3a83d722802f7ea07805da69c8bb
alanurbilgili <alanur123456789@gmail.com>
2021-05-06 16:50:46 +0300

    fixed negative number output

--- DiceRoll.js
+++ DiceRoll.js
@@ -1,10 +1,13 @@
-function DieRoll(){
-   var dNumber=Number(document.getElementById("maxDice").value);
-    if(dNumber <= 0){
+function DieRoll() {
+    var dNumber = Number(document.getElementById("maxDice").value);
+    if (dNumber <= 0) {
         alert("Please type in a postive number.");
     }
-   dNumber=Math.floor(dNumber);
-   var result =  Math.floor(Math.random() * dNumber) + 1;
-   document.getElementById("result").innerHTML = result;
+    else {
+        dNumber = Math.floor(dNumber);
+        result = Math.floor(Math.random() * dNumber) + 1;
+        document.getElementById("result").innerHTML = result;
+    }
+
 
 }
