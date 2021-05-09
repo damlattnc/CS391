05859808215b43229f3880195c41fc6c219c6bb1
damlatutuncu <damlatutun@gmail.com>
2021-05-09 04:38:48 +0300

    Login System updated

--- LoginScreen.js
+++ LoginScreen.js
@@ -1,22 +1,37 @@
 function store() {
+    var Users = ["damla", "alanur", "murat"];
+    var Passwords = ["1234", "12345", "abcd"]
+    localStorage.setItem("Users", JSON.stringify(Users))
+
     var name = document.getElementById('username').value;
     var pw = document.getElementById('password').value;
-    localStorage.setItem('username', name.value);
-    localStorage.setItem('password', pw.value);
+    Users.push(name);
+    Passwords.push(pw);
+
+    alert(name + "  Thanks for registration. \nTry to login Now");
 }
 
-function check() {
+function login() {
 
-    var storedName = localStorage.getItem('username').value;
-    var storedPassword = localStorage.getItem('password').value;
-    var userName = document.getElementById('registeredUser').value;
-    var userPassword = document.getElementById('registeredPw').value;
+    var Users = JSON.parse(localStorage.getItem("Users"));
+    var Passwords = JSON.parse(localStorage.getItem("Passwords"));
+    var userName = document.getElementById('registeredUser');
+    var userPassword = document.getElementById('registeredPw');
 
-    if(userName.value == storedName && userPassword.value == storedPassword) {    
+    //admin login
+
+    if(userName == "Admin" && userPassword == "123456"){
+        alert ('You are logged in.');
+    }
+    
+    else{
+
+    for (var i = 0; i < Users.length; i++) {
+    if (userName == Users[i] && userPassword == Passwords[i]) {
+        alert ('You are logged in.');
         window.location.href = 'Characters.html' 
-        alert('You are logged in.');
+        break;
     }
-    else {
-        alert('ERROR.');
+    alert('Login unsuccessful.');
     }
-}
+}}
